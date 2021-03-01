a9d89f6f452bf3bba849ae358d58c66e52ef6023
Georgey <geobro2310@gmail.com>
2021-03-01 20:11:36 +0530

    second commit

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,9 +1,9 @@
 import { container, title } from './styles'
 
-const Header = () => {
-  ;<header css={container}>
+const Header = () => (
+  <header css={container}>
     <h1 css={title}>NextJs Project</h1>
   </header>
-}
+)
 
 export default Header
