77a536966c0b6873464a0a33176d71449914d985
Georgey <geobro2310@gmail.com>
2021-03-04 23:21:18 +0530

    style about page-1

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,42 @@
+import { css, keyframes } from '@emotion/react'
 import Image from 'next/image'
 import Page from '@layouts/page'
+
+const Header = css`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0 2ch;
+`
+
+const Bio = css`
+  margin-top: 5ch;
+
+  p {
+    max-width: 60ch;
+    margin: 0 auto;
+  }
+
+  > p + p {
+    margin-top: 3ch;
+  }
+`
+
+const Footer = css`
+  margin-top: 5ch;
+  margin-right: 5ch;
+  text-align: right;
+  margin-bottom: 5ch;
+`
 
 export default function About() {
   return (
     <Page>
-      <section>
+      <section css={Header}>
         <Image src="/Profile.png" width={300} height={300} />
+        <h2>Georgey V B</h2>
       </section>
-      <section>
+      <section css={Bio}>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Et tortor
@@ -23,7 +52,7 @@ export default function About() {
           arcu dictum varius duis at consectetur.
         </p>
       </section>
-      <section>
+      <section css={Footer}>
         <small>Follow on Twitter @</small>
         <a
           href="https://twitter.com/BrodasGeo"
@@ -34,7 +63,7 @@ export default function About() {
             textDecorationColor: 'rgb(100, 64, 100)',
           }}
         >
-          Georgey V B
+          geobrodas
         </a>
       </section>
     </Page>
